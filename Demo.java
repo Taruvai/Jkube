101353a518e9c70ec6381b1219fe54a2f7266398
Taruvai Subramaniam <subramaniam.taruvai@gmail.com>
2021-04-14 12:08:19 -0400

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,13 @@
+package com.example.jkube;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Demo {
+	@GetMapping
+	  public String hello() {
+	    return  "Hello from Jkube resouce demo\n";
+	  }
+
+}
---
+++ JKubeResourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jkube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JKubeResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JKubeResourceApplication.class, args);
+	}
+
+}
